64d614a99af062314a0caeea15d018664092a696
Koen Kooi <koen@beagleboard.org>
2009-03-02 13:57:31 -0800

    OMAP3: PM: CPUfreq support for OMAP3EVM board
    From: Koen Kooi <koen@beagleboard.org>
    
    Uses the common OMAP3 OPP settings on OMAP3 EVM board.
    
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-omap2/board-omap3evm.c
+++ arch/arm/mach-omap2/board-omap3evm.c
@@ -42,10 +42,14 @@
 #include <plat/common.h>
 #include <plat/mcspi.h>
 #include <plat/display.h>
+#include <plat/clock.h>
+#include <plat/omap-pm.h>
 
 #include "mux.h"
 #include "sdram-micron-mt46h32m32lf-6.h"
 #include "hsmmc.h"
+#include "pm.h"
+#include "omap3-opp.h"
 
 #define OMAP3_EVM_TS_GPIO	175
 #define OMAP3_EVM_EHCI_VBUS	22
@@ -626,7 +630,8 @@ static void __init omap3_evm_init_irq(void)
 {
 	omap_board_config = omap3_evm_config;
 	omap_board_config_size = ARRAY_SIZE(omap3_evm_config);
-	omap2_init_common_hw(mt46h32m32lf6_sdrc_params, NULL, NULL, NULL, NULL);
+	omap2_init_common_hw(mt46h32m32lf6_sdrc_params, NULL, omap3_mpu_rate_table,
+	                     omap3_dsp_rate_table, omap3_l3_rate_table);
 	omap_init_irq();
 	omap_gpio_init();
 }
