24749bd3b195fdd84d967ad5817fa244da9d3b84
Nishanth Menon <nm@ti.com>
2009-11-10 17:06:08 -0600

    omap3: pm: introduce enabled flag to omap_opp
    We used to enable and disable OPPs based on rate being set to 0, this
    has been confusing in general. So, we now allow specific OPPs to be
    now enabled/disabled by an explicit enabled flag instead of re-using
    rate flag itself.
    
    Cc: Benoit Cousson <b-cousson@ti.com>
    Cc: Eduardo Valentin <eduardo.valentin@nokia.com>
    Cc: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Madhusudhan Chikkature Rajashekar <madhu.cr@ti.com>
    Cc: Paul Walmsley <paul@pwsan.com>
    Cc: Romit Dasgupta <romit@ti.com>
    Cc: Sanjeev Premi <premi@ti.com>
    Cc: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Cc: Sergio Alberto Aguirre Rodriguez <saaguirre@ti.com>
    Cc: Tero Kristo <Tero.Kristo@nokia.com>
    Cc: Thara Gopinath <thara@ti.com>
    Cc: Vishwanath Sripathy <vishwanath.bs@ti.com>
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/omap3-opp.h
+++ arch/arm/mach-omap2/omap3-opp.h
@@ -22,41 +22,41 @@
 #define S166M   166000000
 
 static struct omap_opp omap3_mpu_rate_table[] = {
-	{0, 0, 0},
+	{0, 0, 0, 0},
 	/*OPP1*/
-	{S125M, VDD1_OPP1, 0x1E},
+	{true, S125M, VDD1_OPP1, 0x1E},
 	/*OPP2*/
-	{S250M, VDD1_OPP2, 0x26},
+	{true, S250M, VDD1_OPP2, 0x26},
 	/*OPP3*/
-	{S500M, VDD1_OPP3, 0x30},
+	{true, S500M, VDD1_OPP3, 0x30},
 	/*OPP4*/
-	{S550M, VDD1_OPP4, 0x36},
+	{true, S550M, VDD1_OPP4, 0x36},
 	/*OPP5*/
-	{S600M, VDD1_OPP5, 0x3C},
+	{true, S600M, VDD1_OPP5, 0x3C},
 };
 
 static struct omap_opp omap3_l3_rate_table[] = {
-	{0, 0, 0},
+	{0, 0, 0, 0},
 	/*OPP1*/
-	{0, VDD2_OPP1, 0x1E},
+	{false, 0, VDD2_OPP1, 0x1E},
 	/*OPP2*/
-	{S83M, VDD2_OPP2, 0x24},
+	{true, S83M, VDD2_OPP2, 0x24},
 	/*OPP3*/
-	{S166M, VDD2_OPP3, 0x2C},
+	{true, S166M, VDD2_OPP3, 0x2C},
 };
 
 static struct omap_opp omap3_dsp_rate_table[] = {
-	{0, 0, 0},
+	{0, 0, 0, 0},
 	/*OPP1*/
-	{S90M, VDD1_OPP1, 0x1E},
+	{true, S90M, VDD1_OPP1, 0x1E},
 	/*OPP2*/
-	{S180M, VDD1_OPP2, 0x26},
+	{true, S180M, VDD1_OPP2, 0x26},
 	/*OPP3*/
-	{S360M, VDD1_OPP3, 0x30},
+	{true, S360M, VDD1_OPP3, 0x30},
 	/*OPP4*/
-	{S400M, VDD1_OPP4, 0x36},
+	{true, S400M, VDD1_OPP4, 0x36},
 	/*OPP5*/
-	{S430M, VDD1_OPP5, 0x3C},
+	{true, S430M, VDD1_OPP5, 0x3C},
 };
 
 #endif
