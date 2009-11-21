6f27d039e6c8485131c620398c41be8cc8447fdd
Ajay Kumar Gupta <ajay.gupta@ti.com>
2009-11-21 17:20:39 +0530

    musb: start session only if A-device
    Currently session is always started when image is OTG build.This
    way Vbus is enabled always and gets switched off only when a device
    is connected and then disconnected.
    
    Added a A-device check from DEVCTL register to start session.
    
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -923,7 +923,7 @@ void musb_start(struct musb *musb)
 		 */
 		if ((devctl & MUSB_DEVCTL_VBUS) == MUSB_DEVCTL_VBUS)
 			musb->is_active = 1;
-		else
+		else if (!cpu_is_omap3517() && !cpu_is_omap3505())
 			devctl |= MUSB_DEVCTL_SESSION;
 
 	} else if (is_host_enabled(musb)) {
