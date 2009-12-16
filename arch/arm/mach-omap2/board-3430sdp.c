d5cf0c47bcf56ea220661c333edad3832ff46d7d
Kevin Hilman <khilman@deeprootsystems.com>
2009-12-15 16:37:18 -0800

    OMAP3: PM: Configure CPUidle latencies/thresholds from board file
    The CPUidle C state latencies and thresholds are dependent on various
    board specific details.  This patch makes it possible to configure
    these values from the respective board files.
    
    omap3_pm_init_cpuidle() can now be optionally called from board files
    to pass board specific cpuidle parameters.  If the board files do not
    use this function to pass the params default values are used which
    might cause higher consumption dur to wrong state selection by the
    governor.
    
    This patch only updates the 3430sdp board files to use
    omap3_pm_init_cpuidle().
    
    From Kalle, in addition to original patch from Rajendra:
    
    Building without CONFIG_CPU_IDLE or CONFIG_PM causes build to fail if
    cpu idle parameters are tried to pass using omap3_pm_init_cpuidle
    function.
    
    Signed-off-by: Rajendra Nayak <rnayak@ti.com>
    Signed-off-by: Kalle Jokiniemi <kalle.jokiniemi@digia.com>
    Signed-off-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-omap2/board-3430sdp.c
+++ arch/arm/mach-omap2/board-3430sdp.c
@@ -44,6 +44,7 @@
 #include "mux.h"
 #include "sdram-qimonda-hyb18m512160af-6.h"
 #include "mmc-twl4030.h"
+#include "pm.h"
 
 #define CONFIG_DISABLE_HFCLK 1
 
@@ -54,6 +55,24 @@
 #define ENABLE_VAUX3_DEV_GRP	0x20
 
 #define TWL4030_MSECURE_GPIO 22
+
+/* FIXME: These values need to be updated based on more profiling on 3430sdp*/
+static struct cpuidle_params omap3_cpuidle_params_table[] = {
+	/* C1 */
+	{2, 2, 5},
+	/* C2 */
+	{10, 10, 30},
+	/* C3 */
+	{50, 50, 300},
+	/* C4 */
+	{1500, 1800, 4000},
+	/* C5 */
+	{2500, 7500, 12000},
+	/* C6 */
+	{3000, 8500, 15000},
+	/* C7 */
+	{10000, 30000, 300000},
+};
 
 static int board_keymap[] = {
 	KEY(0, 0, KEY_LEFT),
@@ -305,6 +324,7 @@ static void __init omap_3430sdp_init_irq(void)
 {
 	omap_board_config = sdp3430_config;
 	omap_board_config_size = ARRAY_SIZE(sdp3430_config);
+	omap3_pm_init_cpuidle(omap3_cpuidle_params_table);
 	omap2_init_common_hw(hyb18m512160af6_sdrc_params, NULL);
 	omap_init_irq();
 	omap_gpio_init();
