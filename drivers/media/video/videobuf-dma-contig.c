83913e7b0613f6bc3955d9554e9fc6bf6b142ec6
Vaibhav Hiremath <hvaibhav@ti.com>
2010-01-08 20:34:11 +0530

    Videobuf-dma-contig: Set PAGE_READONLY flag for capture mmap
    This will be maintained as custom patch till the time we don't get proper
    resolution, we are following up this issue with Commuinty.

--- drivers/media/video/videobuf-dma-contig.c
+++ drivers/media/video/videobuf-dma-contig.c
@@ -337,7 +337,7 @@ static int __videobuf_mmap_mapper(struct videobuf_queue *q,
 	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 	retval = remap_pfn_range(vma, vma->vm_start,
 				 mem->dma_handle >> PAGE_SHIFT,
-				 size, vma->vm_page_prot);
+				 size, vma->vm_page_prot | PAGE_READONLY);
 	if (retval) {
 		dev_err(q->dev, "mmap: remap failed with error %d. ", retval);
 		dma_free_coherent(q->dev, mem->size,
