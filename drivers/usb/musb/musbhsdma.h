bcd6f414a8616f180e33db93a86d48195ae93bdf
Ajay Kumar Gupta <ajay.gupta@ti.com>
2010-05-05 19:28:19 +0530

    musb: sdma: Revert use optimal transfer element
    This patch is affetcing MSC host class operation on OMAP3EVM.
    So revert this for now.

--- drivers/usb/musb/musbhsdma.h
+++ drivers/usb/musb/musbhsdma.h
@@ -40,15 +40,15 @@
 #endif
 
 #ifdef CONFIG_MUSB_USE_SYSTEM_DMA_RX
-static int use_sdma = 1;
+static int is_sdma = 1;
 #else
-#define use_sdma          0
+#define is_sdma          0
 #endif
 
 int use_system_dma(u8 tx)
 {
 	int ret = 0;
-	if (use_sdma && (cpu_is_omap3630() || (cpu_is_omap3430() && !tx)))
+	if (is_sdma && (cpu_is_omap3630() || (cpu_is_omap3430() && !tx)))
 		ret = 1;
 
 	return ret;
