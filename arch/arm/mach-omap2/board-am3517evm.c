44f715ff7ca1032ad0a0455f72c6c271ec79ba6c
Vaibhav Hiremath <hvaibhav@ti.com>
2010-05-28 16:34:41 +0530

    AM3517: Add support for TSC2004 driver

--- arch/arm/mach-omap2/board-am3517evm.c
+++ arch/arm/mach-omap2/board-am3517evm.c
@@ -23,6 +23,8 @@
 #include <linux/i2c/pca953x.h>
 #include <linux/can/platform/ti_hecc.h>
 #include <linux/davinci_emac.h>
+#include <linux/irq.h>
+#include <linux/i2c/tsc2004.h>
 
 #include <mach/hardware.h>
 #include <mach/am35xx.h>
@@ -355,6 +357,59 @@ static struct i2c_board_info __initdata am3517evm_i2c3_boardinfo[] = {
 	},
 };
 
+/*
+ * TSC 2004 Support
+ */
+#define	GPIO_TSC2004_IRQ	65
+
+static int tsc2004_init_irq(void)
+{
+	int ret = 0;
+
+	ret = gpio_request(GPIO_TSC2004_IRQ, "tsc2004-irq");
+	if (ret < 0) {
+		printk(KERN_WARNING "failed to request GPIO#%d: %d\n",
+				GPIO_TSC2004_IRQ, ret);
+		return ret;
+	}
+
+	if (gpio_direction_input(GPIO_TSC2004_IRQ)) {
+		printk(KERN_WARNING "GPIO#%d cannot be configured as "
+				"input\n", GPIO_TSC2004_IRQ);
+		return -ENXIO;
+	}
+
+	omap_set_gpio_debounce(GPIO_TSC2004_IRQ, 1);
+	omap_set_gpio_debounce_time(GPIO_TSC2004_IRQ, 0xa);
+	return ret;
+}
+
+static void tsc2004_exit_irq(void)
+{
+	gpio_free(GPIO_TSC2004_IRQ);
+}
+
+static int tsc2004_get_irq_level(void)
+{
+	return gpio_get_value(GPIO_TSC2004_IRQ) ? 0 : 1;
+}
+
+struct tsc2004_platform_data am3517evm_tsc2004data = {
+	.model = 2004,
+	.x_plate_ohms = 180,
+	.get_pendown_state = tsc2004_get_irq_level,
+	.init_platform_hw = tsc2004_init_irq,
+	.exit_platform_hw = tsc2004_exit_irq,
+};
+
+static struct i2c_board_info __initdata am3517evm_tsc_i2c_boardinfo[] = {
+	{
+		I2C_BOARD_INFO("tsc2004", 0x4B),
+		.type		= "tsc2004",
+		.platform_data	= &am3517evm_tsc2004data,
+	},
+};
+
 static int __init am3517_evm_i2c_init(void)
 {
 	omap_register_i2c_bus(1, 400, NULL, 0);
@@ -619,6 +674,12 @@ static void __init am3517_evm_init(void)
 			&vpfe_capture_dev.dev);
 	clk_add_alias("slave", "dm644x_ccdc", "slave",
 			&vpfe_capture_dev.dev);
+
+	/* TSC 2004 */
+	omap_mux_init_gpio(65, OMAP_PIN_INPUT_PULLUP);
+	am3517evm_tsc_i2c_boardinfo[0].irq = gpio_to_irq(GPIO_TSC2004_IRQ);
+	i2c_register_board_info(1, am3517evm_tsc_i2c_boardinfo,
+				ARRAY_SIZE(am3517evm_tsc_i2c_boardinfo));
 }
 
 static void __init am3517_evm_map_io(void)
