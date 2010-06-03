05e36d91aac22b9781f48f3aa01d4ece27193f43
Nicolas Boichat <nicolas@boichat.ch>
2010-06-03 20:31:11 +0200

    Fix BeagleBoard platform data, preventing MUSB from being used in Peripheral-only mode.

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -479,7 +479,13 @@ static struct omap_board_mux board_mux[] __initdata = {
 
 static struct omap_musb_board_data musb_board_data = {
 	.interface_type		= MUSB_INTERFACE_ULPI,
+#ifdef CONFIG_USB_MUSB_OTG
 	.mode			= MUSB_OTG,
+#elif defined(CONFIG_USB_MUSB_HDRC_HCD)
+	.mode			= MUSB_HOST,
+#elif defined(CONFIG_USB_GADGET_MUSB_HDRC)
+	.mode			= MUSB_PERIPHERAL,
+#endif
 	.power			= 100,
 };
 
